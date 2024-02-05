bd0bd8baf65b44dff4acefca457848e6043e2cb8
Yasserelf <145704126+Yasserelf@users.noreply.github.com>
2024-02-05 20:29:53 +0100

    Update Staff.java

--- src/Staff/Staff.java
+++ src/Staff/Staff.java
@@ -1,17 +1,42 @@
 package Staff;
 
+/**
+ * The abstract base class for different types of staff members.
+ * It provides a mechanism to assign a unique identifier to each staff instance.
+ */
 public abstract class Staff {
-    protected String type ;
-    private static int idCounter=1 ; 
+    /**
+     * The type of the staff member.
+     */
+    protected String type;
+
+    /**
+     * A unique identifier for each staff member.
+     */
+    private static int idCounter = 1;
+
+    /**
+     * The unique identifier for this staff member.
+     */
     protected int id;
 
+    /**
+     * Constructs a new staff member with the given type.
+     * The unique identifier is automatically assigned.
+     *
+     * @param t The type of the staff member.
+     */
     public Staff(String t) {
-        this.type = t ; 
-        this.id = idCounter++; 
+        this.type = t;
+        this.id = idCounter++;
     }
 
+    /**
+     * Gets the type of the staff member.
+     *
+     * @return The type of the staff member.
+     */
     public String getType() {
         return this.type;
     }
-
 }
