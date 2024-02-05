f81f40dbd1307a012cdfc65941f3cf080b919f0b
Yasserelf <145704126+Yasserelf@users.noreply.github.com>
2024-02-05 20:30:56 +0100

    Update RedistributionStrategy.java

--- src/Strategie/RedistributionStrategy.java
+++ src/Strategie/RedistributionStrategy.java
@@ -5,12 +5,23 @@ import java.util.List;
 import Exceptions.EmptySpaceException;
 import Exceptions.FullStationException;
 import Exceptions.OutOfServiceException;
-import Rentable.Rentable;
 import Station.Station;
 
+/**
+ * An interface representing a redistribution strategy for stations.
+ * Implementations of this interface define how the redistribution of rentable items
+ * is performed among a list of stations.
+ */
 public interface RedistributionStrategy {
 
+    /**
+     * Redistributes rentable items among a list of stations.
+     *
+     * @param stations The list of stations to redistribute rentable items among.
+     * @throws EmptySpaceException   If there is an empty space exception during redistribution.
+     * @throws OutOfServiceException If there is an out-of-service exception during redistribution.
+     * @throws FullStationException  If there is a full station exception during redistribution.
+     */
     void redistribute(List<Station> stations)
             throws EmptySpaceException, OutOfServiceException, FullStationException;
-
 }
